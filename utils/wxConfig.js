9afd15b800336c9c0005313b628cd0275162dbc5
宋迎春 <songyingchun@harsons.cn>
2022-12-29 16:21:15 +0800

    init: 项目初始化

---
+++ utils/wxConfig.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: yuyongxing
+ * @Date: 2022-04-08 10:40:07
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-12-29 15:35:37
+ * @Description: 
+ */
+module.exports = {
+    appid:"wx67fc5f36e1f745d2",
+    secret:"484c4d9f8d1ae58397b9e48148906897",
+    token:"token"
+}
